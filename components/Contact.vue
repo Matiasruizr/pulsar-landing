f6abad558adaec4bc33bf5f6e82b3623d4af933e
Jose <jmmontes@uc.cl>
2019-12-23 17:24:54 -0300

    contactform

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,14 +1,13 @@
 <template>
   <section id="contact"  class="section is-medium" style="background-color: rgba(245, 245, 245, 1)">
-    <div class="container has-text-centered"  align="center" fluid style="padding: 30%">
+    <div class="container has-text-centered"  align="center">
       <h1 class="title is-spaced">Contáctanos</h1>
       <h4 class="subtitle">Construyamos algo increible</h4>
               <p
                 class="mt-0"
               >Tu proyecto es muy importante para nosotros, contactanos y te responderemos a la brevedad.</p>
 
-      <form action="https://formspree.io/pulsar.dev.team@gmail.com" method="POST">
-        <div class="field">
+        <div class="field" fluid style="padding: 30%">
           <label class="label" for="name">Nombre</label>
           <div class="control">
             <input v-validate="'required'" v-model="name" class="input" :class="{ 'is-danger': errors.has('name') }" id="name" name="name" type="text" placeholder="Nombre" required>
@@ -16,7 +15,7 @@
           <span v-show="errors.has('name')" class="help is-danger">{{ errors.first('name') }}</span>
         </div>
 
-        <div class="field">
+        <div class="field" fluid style="padding: 30%">
           <label class="label" for="email">Email</label>
           <div class="control has-icons-left has-icons-right">
             <input v-validate="'required|email'" v-model="email" class="input" :class="{ 'is-danger': errors.has('email') }" id="email" name="email" type="text" placeholder="Email" required>
@@ -26,7 +25,7 @@
           <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
         </div>
 
-        <div class="field">
+        <div class="field" fluid style="padding: 30%">
           <label class="label" for="message">Mensaje</label>
           <div class="control">
             <textarea v-validate="'required'" v-model="message" class="textarea" :class="{ 'is-danger': errors.has('message') }" id="message" name="message" placeholder="Mensaje" required></textarea>
@@ -34,7 +33,7 @@
           <span v-show="errors.has('message')" class="help is-danger">{{ errors.first('message') }}</span>
         </div>
 
-        <div class="field is-grouped">
+        <div class="field is-grouped" fluid style="padding: 30%">
           <div class="control">
             <input type="submit" class="button is-primary" value="Enviar">
           </div>
@@ -42,7 +41,6 @@
             <button class="button is-light" @click="cancel">Cancelar</button>
           </div>
         </div>
-      </form>
     </div>
   </section>
 </template>
